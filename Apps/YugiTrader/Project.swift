fb031ad586ea4e54d52f5535050b0b963ccbc2db
valselee <sollleky72@gmail.com>
2023-05-24 11:13:12 +0900

    [Del] 쓰지 않을 프로젝트 파일 삭제

--- Apps/YugiTrader/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-
-enum Constants {
-    static let ORG_NAME = "Celan"
-    static let APP_NAME = "YugiTrader"
-}
-
-let project = Project(name: Constants.APP_NAME)
