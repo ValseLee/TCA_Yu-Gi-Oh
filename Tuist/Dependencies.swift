7d5d85cc4dd0b9b032f91da9d69a2e140bde6804
valselee <sollleky72@gmail.com>
2023-05-28 17:23:51 +0900

    [Tuist/#3] - TargetDependency, Package, SettingsDictionary의 확장 구현 - firebase setting을 돕는 Setting 옵션 구현 - mainApp, app 메소드를 분리하여 의존성을 갖는 프로젝트 간 구별

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -1,14 +1,10 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 
 let dependencies = Dependencies(
     swiftPackageManager: .init([
-        .remote(
-            url: "https://github.com/pointfreeco/swift-composable-architecture",
-            requirement: .branch("main")
-        ),
-        .remote(
-            url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
-            requirement: .upToNextMinor(from: "4.6.3")
-        )
+        .sendbirdSDK,
+        .composableArchitecture,
+        .firebase
     ])
 )
--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,6 +1,84 @@
 import ProjectDescription
 
+// MARK: - TARGET DEPENDENCY > EXTERNAL
+public extension TargetDependency {
+    static let firebaseMessaging: Self = .external(name: "FirebaseMessaging")
+    static let sendbirdChatSDK: Self = .external(name: "SendbirdChatSDK")
+    static let composableArchitecture: Self = .external(name: "ComposableArchitecture")
+}
+
+// MARK: - TARGET DEPENDENCY > PROJECT
+public extension TargetDependency {
+    static let commonModule: Self = .project(
+        target: "CommonModule",
+        path: .relativeToRoot("Modules/CommonModule")
+    )
+    
+    static let yugiTraderChat: Self = .project(
+        target: "YugiTraderChat",
+        path: .relativeToRoot("Apps/YugiTraderChat")
+    )
+    static let yugiTraderLogin: Self = .project(
+        target: "YugiTraderLogin",
+        path: .relativeToRoot("Apps/YugiTraderLogin")
+    )
+    static let yugiTraderAppInternal: Self = .project(
+        target: "YugiTraderAppInternal",
+        path: .relativeToRoot("Modules/YugiTraderAppInternal")
+    )
+}
+
+/**
+ SPM 에서 Resolve 할 Package Target을 설정합니다.
+ 모든 Package는 Dependencies 파일에서 관리하며, 프로젝트는 external로 접근하여 패키지 타겟을 빌드합니다.
+ */
+public extension Package {
+    static let firebase: Package = .remote(
+        url: "https://github.com/firebase/firebase-ios-sdk",
+        requirement: .upToNextMajor(from: "9.0.0")
+    )
+    
+    static let composableArchitecture: Package = .remote(
+        url: "https://github.com/pointfreeco/swift-composable-architecture",
+        requirement: .branch("main")
+    )
+    
+    static let sendbirdSDK: Package = .remote(
+        url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
+        requirement: .upToNextMinor(from: "4.6.3")
+    )
+}
+
+public extension SettingsDictionary {
+    static let firebase: Self = [
+        "DEBUG_INFORMATION_FORMAT": "dwarf-with-dsym",
+        "OTHER_LDFLAGS": "-ObjC"
+    ]
+}
+
 extension Project {
+    /**
+     App의 @main, IntroPoint를 설정하는 메소드
+     */
+    public static func mainApp(name: String) -> Project {
+        Project(
+            name: name,
+            organizationName: "Celan",
+            targets: [
+                makeAppTarget(
+                    name: name,
+                    platform: .iOS,
+                    dependencies: [
+                        .commonModule,
+                        .yugiTraderAppInternal,
+                        .yugiTraderChat,
+                        .yugiTraderLogin
+                    ]
+                )
+            ]
+        )
+    }
+    
     /**
      해당하는 모든 프로젝트 타겟에 공통으로 CommonModule 의존성 주입
      */
@@ -13,16 +91,8 @@ extension Project {
                     name: name,
                     platform: .iOS,
                     dependencies: [
-                        // Common Module For Design System
-                        .project(
-                            target: "CommonModule",
-                            path: .relativeToRoot("Modules/CommonModule")
-                        ),
-                        // Internal Module For Settings/Plists
-                        .project(
-                            target: "Internal",
-                            path: .relativeToRoot("Modules/Internal")
-                        )
+                        .commonModule,
+                        .yugiTraderAppInternal
                     ]
                 )
             ]
@@ -55,7 +125,8 @@ extension Project {
             dependencies: dependencies,
             settings: .settings(
                 configurations: [
-                    // Configurations
+                    .debug(name: .debug, settings: .firebase),
+                    .release(name: .release, settings: .firebase)
                 ],
                 defaultSettings: .recommended
             )
