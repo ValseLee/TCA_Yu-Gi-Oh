091c6cfb4b682489c2d86f55d334a78d268dd4c7
valselee <sollleky72@gmail.com>
2023-05-28 17:21:31 +0900

    [Tuist/#3] Tuist Source 폴더에 기본 파일 추가, 브릿징헤더 추가

---
+++ Apps/YugiTraderLogin/Sources/YTLoginView.swift
@@ -0,0 +1,21 @@
+//
+//  YTLoginView.swift
+//  YugiTraderLogin
+//
+//  Created by Celan on 2023/05/28.
+//  Copyright © 2023 Celan. All rights reserved.
+//
+
+import SwiftUI
+
+struct YTLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YTLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        YTLoginView()
+    }
+}
