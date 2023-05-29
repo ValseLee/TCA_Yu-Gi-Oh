16ae5f18582a5aaff381ac31d51b9bcf8fe5c71c
valselee <sollleky72@gmail.com>
2023-05-29 16:54:11 +0900

    [Tuist/#3] mainApp, subApp 의존성 정의, build setting 정의, subApp target을 framework로 설정하여 의존성 충돌 해결

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -7,24 +7,46 @@ public extension TargetDependency {
     static let composableArchitecture: Self = .external(name: "ComposableArchitecture")
 }
 
-// MARK: - TARGET DEPENDENCY > PROJECT
+// MARK: - TARGET DEPENDENCY > FRAMEWORKS
 public extension TargetDependency {
     static let commonModule: Self = .project(
         target: "CommonModule",
         path: .relativeToRoot("Modules/CommonModule")
     )
     
+    static let yugiTraderAppInternal: Self = .project(
+        target: "YugiTraderAppInternal",
+        path: .relativeToRoot("Modules/YugiTraderAppInternal")
+    )
+    
     static let yugiTraderChat: Self = .project(
         target: "YugiTraderChat",
         path: .relativeToRoot("Apps/YugiTraderChat")
     )
+    
+    static let yugiTraderChatKit: Self = .project(
+        target: "YugiTraderChatKit",
+        path: .relativeToRoot("Modules/YugiTraderChatKit")
+    )
+    
+    static let yugiTraderChatUI: Self = .project(
+        target: "YugiTraderChatUI",
+        path: .relativeToRoot("Modules/YugiTraderChatUI")
+    )
+    
     static let yugiTraderLogin: Self = .project(
         target: "YugiTraderLogin",
         path: .relativeToRoot("Apps/YugiTraderLogin")
     )
-    static let yugiTraderAppInternal: Self = .project(
-        target: "YugiTraderAppInternal",
-        path: .relativeToRoot("Modules/YugiTraderAppInternal")
+    
+    static let yugiTraderLoginKit: Self = .project(
+        target: "YugiTraderLoginKit",
+        path: .relativeToRoot("Modules/YugiTraderLoginKit")
+    )
+    
+    static let yugiTraderLoginUI: Self = .project(
+        target: "YugiTraderLoginUI",
+        path: .relativeToRoot("Modules/YugiTraderLoginUI")
     )
 }
 
@@ -33,17 +55,17 @@ public extension TargetDependency {
  모든 Package는 Dependencies 파일에서 관리하며, 프로젝트는 external로 접근하여 패키지 타겟을 빌드합니다.
  */
 public extension Package {
-    static let firebase: Package = .remote(
+    static let firebase: Self = .remote(
         url: "https://github.com/firebase/firebase-ios-sdk",
         requirement: .upToNextMajor(from: "9.0.0")
     )
     
-    static let composableArchitecture: Package = .remote(
+    static let composableArchitecture: Self = .remote(
         url: "https://github.com/pointfreeco/swift-composable-architecture",
         requirement: .branch("main")
     )
     
-    static let sendbirdSDK: Package = .remote(
+    static let sendbirdSDK: Self = .remote(
         url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
         requirement: .upToNextMinor(from: "4.6.3")
     )
@@ -60,7 +82,10 @@ extension Project {
     /**
      App의 @main, IntroPoint를 설정하는 메소드
      */
-    public static func mainApp(name: String) -> Project {
+    public static func mainApp(
+        name: String,
+        targetDependencies: [TargetDependency]
+    ) -> Project {
         Project(
             name: name,
             organizationName: "Celan",
@@ -68,31 +93,50 @@ extension Project {
                 makeAppTarget(
                     name: name,
                     platform: .iOS,
-                    dependencies: [
-                        .commonModule,
-                        .yugiTraderAppInternal,
-                        .yugiTraderChat,
-                        .yugiTraderLogin
-                    ]
-                )
+                    dependencies: targetDependencies
+                ),
             ]
         )
     }
     
     /**
-     해당하는 모든 프로젝트 타겟에 공통으로 CommonModule 의존성 주입
+     subApp의 하위 모듈을 framework로 repackaging 하는 메소드.
+     mainApp은 subApp을 참조한다.
      */
-    public static func app(name: String) -> Project {
+    public static func subApp(
+        name: String,
+        targetDependencies: [TargetDependency]
+    ) -> Project {
         Project(
             name: name,
             organizationName: "Celan",
             targets: [
-                makeAppTarget(
+                makeFrameworkTarget(
+                    name: name,
+                    platform: .iOS,
+                    dependencies: targetDependencies
+                ),
+            ]
+        )
+    }
+    
+    /**
+     해당하는 모든 프레임워크 타겟에 공통으로 CommonModule, Internal 설정 의존성 주입
+     */
+    public static func framework(
+        name: String
+//        additionalTargets: [String]
+    ) -> Project {
+        Project(
+            name: name,
+            organizationName: "Celan",
+            targets: [
+                makeFrameworkTarget(
                     name: name,
                     platform: .iOS,
                     dependencies: [
                         .commonModule,
-                        .yugiTraderAppInternal
+                        .yugiTraderAppInternal,
                     ]
                 )
             ]
@@ -104,13 +148,12 @@ extension Project {
         platform: Platform,
         dependencies: [TargetDependency]
     ) -> Target {
-        let platform: Platform = platform
-        
         let infoPlist: [String: InfoPlist.Value] = [
             "CFBundleShortVersionString": "1.0",
             "CFBundleVersion": "1",
             "UIMainStoryboardFile": "",
             "UILaunchStoryboardName": "LaunchScreen",
+            "FirebaseAppDelegateProxyEnabled": "NO"
         ]
 
         let mainTarget = Target(
@@ -124,14 +167,50 @@ extension Project {
             resources: [.glob(pattern: .relativeToManifest("Resources/**"))],
             dependencies: dependencies,
             settings: .settings(
+                base: SettingsDictionary().automaticCodeSigning(devTeam: "67M6ZS7KS6"),
                 configurations: [
                     .debug(name: .debug, settings: .firebase),
-                    .release(name: .release, settings: .firebase)
+                    .release(name: .release, settings: .firebase),
                 ],
                 defaultSettings: .recommended
             )
         )
-        
+
+        return mainTarget
+    }
+    
+    private static func makeFrameworkTarget(
+        name: String,
+        platform: Platform,
+        dependencies: [TargetDependency]
+    ) -> Target {
+        let infoPlist: [String: InfoPlist.Value] = [
+            "CFBundleShortVersionString": "1.0",
+            "CFBundleVersion": "1",
+            "UIMainStoryboardFile": "",
+            "UILaunchStoryboardName": "LaunchScreen",
+            "FirebaseAppDelegateProxyEnabled": "NO"
+        ]
+
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .framework,
+            bundleId: "com.Celan.\(name)",
+            deploymentTarget: .iOS(targetVersion: "16.0", devices: .iphone),
+            infoPlist: .extendingDefault(with: infoPlist),
+            sources: [.glob(.relativeToManifest("Sources/**"))],
+            resources: [.glob(pattern: .relativeToManifest("Resources/**"))],
+            dependencies: dependencies,
+            settings: .settings(
+                base: SettingsDictionary().automaticCodeSigning(devTeam: "67M6ZS7KS6"),
+                configurations: [
+                    .debug(name: .debug, settings: .firebase),
+                    .release(name: .release, settings: .firebase),
+                ],
+                defaultSettings: .recommended
+            )
+        )
         return mainTarget
     }
 }
