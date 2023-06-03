b141feb14878b7b94d040332851296507a87a881
valselee <sollleky72@gmail.com>
2023-06-03 23:34:00 +0900

    [Add/#2] - ChatKit > YTChatStore 파일 생성 - ChatUI > MainYTChatRoomView 파일 생성

---
+++ Modules/YugiTraderChatKit/Sources/YTChatStore.swift
@@ -0,0 +1,21 @@
+//
+//  YTChatStore.swift
+//  YugiTraderChatKit
+//
+//  Created by Celan on 2023/06/03.
+//  Copyright © 2023 Celan. All rights reserved.
+//
+
+import SwiftUI
+
+struct YTChatStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YTChatStore_Previews: PreviewProvider {
+    static var previews: some View {
+        YTChatStore()
+    }
+}
---
+++ Modules/YugiTraderChatUI/Sources/MainYTChatRoomView.swift
@@ -0,0 +1,21 @@
+//
+//  MainYTChatRoomView.swift
+//  YugiTraderChatUI
+//
+//  Created by Celan on 2023/06/03.
+//  Copyright © 2023 Celan. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainYTChatRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainYTChatRoomView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainYTChatRoomView()
+    }
+}
