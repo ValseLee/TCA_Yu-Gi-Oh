a680cbee1cb70e540aed311a57832d6e7579e410
valselee <sollleky72@gmail.com>
2023-06-05 14:39:14 +0900

    [Chore/#2] TCA 연습 코드를 Kit 의존성 모듈로 이동

--- Modules/YugiTraderChatKit/Sources/YTChatStore.swift
+++ Modules/YugiTraderChatKit/Sources/YTChatStore.swift
@@ -7,15 +7,50 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct YTChatStore: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+public enum StateFilter: String {
+    case all, proceeding, completed
 }
 
-struct YTChatStore_Previews: PreviewProvider {
-    static var previews: some View {
-        YTChatStore()
+public struct ChatStateStore: ReducerProtocol {
+    public struct State: Equatable {
+        public var chatStateFilter: StateFilter = .all
+        public var name: String
+        
+        public init(
+            chatStateFilter: StateFilter,
+            name: String
+        ) {
+            self.chatStateFilter = chatStateFilter
+            self.name = name
+        }
+    }
+    
+    public enum Action: Equatable {
+        case sendChat
+        case afterSendChat
+        case changeName
+    }
+    
+    public init() { }
+    
+    public var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .sendChat:
+                state.chatStateFilter = .proceeding
+                print("CHAT IS NOW SENDING")
+                return .send(.afterSendChat)
+                
+            case .afterSendChat:
+                state.chatStateFilter = .completed
+                return .send(.changeName)
+            
+            case .changeName:
+                state.name = state.chatStateFilter.rawValue
+                return .none
+            }
+        }
     }
 }
