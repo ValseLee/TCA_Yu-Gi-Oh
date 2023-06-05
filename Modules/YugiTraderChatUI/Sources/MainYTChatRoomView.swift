532154bafd5d5784250de8226136c752003c9ee1
valselee <sollleky72@gmail.com>
2023-06-05 14:37:40 +0900

    [Update/#2] ScrollView, ScrollViewReader Mock View 구현, 날짜를 누르면 해당 날짜의 메시지로 이동하는 기능 실험중

--- Modules/YugiTraderChatUI/Sources/MainYTChatRoomView.swift
+++ Modules/YugiTraderChatUI/Sources/MainYTChatRoomView.swift
@@ -7,15 +7,85 @@
 //
 
 import SwiftUI
+import CommonModule
+import YugiTraderAppInternal
 
 struct MainYTChatRoomView: View {
+    @State private var scrollOffset: CGFloat = 0
+    @State private var date: Date = .now
+    
+    let mockArr: [Message] = Array(
+        repeating: Message.mock(),
+        count: 20
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            DatePicker(
+                  "DatePicker",
+                  selection: $date,
+                  displayedComponents: [.date]
+            )
+            .padding(.horizontal)
+            
+            ScrollViewReader { reader in
+                VStack {
+                    ForEach(
+                        Array(mockArr.enumerated()),
+                        id: \.offset
+                    ) { index, message in
+                        Text("\(index), \(message.message)")
+                            .padding(8)
+                            .frame(maxWidth: .infinity)
+                            .id(Date.getMessageDateString(with: message.date))
+                        
+                        Text(Date.getMessageDateString(with: message.date))
+                    }
+                }
+                .onChange(of: date) { newValue in
+                    print("DATE CHANGED: \(newValue.formatted())")
+                    reader.scrollTo(
+                        Date.getMessageDateString(with: newValue),
+                        anchor: .bottom
+                    )
+                }
+            }
+        }
     }
 }
 
 struct MainYTChatRoomView_Previews: PreviewProvider {
     static var previews: some View {
         MainYTChatRoomView()
+    }
+}
+
+struct Message: Hashable {
+    let id: String = UUID().uuidString
+    let message: String
+    let date: Date
+    
+    static func mock() -> Self {
+        Message(
+            message: "mock Message",
+            date: .now
+        )
+    }
+}
+
+struct ScrollViewPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat? = nil
+    
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        defaultValue = value ?? nextValue()
+    }
+}
+
+extension Date {
+    static func getMessageDateString(with messageDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let str = dateFormatter.string(from: messageDate)
+        return str
     }
 }
