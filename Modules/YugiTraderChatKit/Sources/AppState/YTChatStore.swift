762521a3ba5c3cafc873cc3a7c19b076d29351e8
valselee <sollleky72@gmail.com>
2023-06-09 23:56:28 +0900

    "[Feat/#2] UI에 포함된 State를 Kit로 이전 - scollDate, scrollHighlight, isCalendarShown - 어떻게 뷰의 데이터를 받고 처리해서 다시 돌려줄 것인지 연구 필요"

--- Modules/YugiTraderChatKit/Sources/AppState/YTChatStore.swift
+++ Modules/YugiTraderChatKit/Sources/AppState/YTChatStore.swift
@@ -17,20 +17,31 @@ public struct ChatStateStore: ReducerProtocol {
     public struct State: Equatable {
         public var chatStateFilter: StateFilter = .all
         public var name: String
+        public var scollDate: Date
+        public var scrollHighlight: String
+        public var isCalendarShown: Bool
         
         public init(
             chatStateFilter: StateFilter,
-            name: String
+            name: String,
+            scollDate: Date,
+            scrollHighlight: String,
+            isCalendarShown: Bool
         ) {
             self.chatStateFilter = chatStateFilter
             self.name = name
+            self.scollDate = scollDate
+            self.scrollHighlight = scrollHighlight
+            self.isCalendarShown = isCalendarShown
         }
     }
     
     public enum Action: Equatable {
-        case sendChat
-        case afterSendChat
+        case sendMessage
+        case afterSendMessage
         case changeName
+        case scrollToSpecificDate
+        case highlightMessageBubble
     }
     
     public init() { }
@@ -38,17 +49,24 @@ public struct ChatStateStore: ReducerProtocol {
     public var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
-            case .sendChat:
+            case .sendMessage:
                 state.chatStateFilter = .proceeding
-                print("CHAT IS NOW SENDING")
-                return .send(.afterSendChat)
+                print("Message IS NOW SENDING")
+                return .send(.afterSendMessage)
                 
-            case .afterSendChat:
+            case .afterSendMessage:
                 state.chatStateFilter = .completed
                 return .send(.changeName)
             
             case .changeName:
                 state.name = state.chatStateFilter.rawValue
+                return .none
+                
+            case .scrollToSpecificDate:
+                
+                return .send(.highlightMessageBubble)
+                
+            case .highlightMessageBubble:
                 return .none
             }
         }
