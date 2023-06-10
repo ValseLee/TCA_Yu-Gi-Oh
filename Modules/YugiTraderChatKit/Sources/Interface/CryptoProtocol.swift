41766b281f6eda72e6fa59466a120f5463bcf852
valselee <sollleky72@gmail.com>
2023-06-10 23:41:15 +0900

    [Chore] Crypt Logic Test

--- Modules/YugiTraderChatKit/Sources/Interface/CryptoProtocol.swift
+++ Modules/YugiTraderChatKit/Sources/Interface/CryptoProtocol.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2023 Celan. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+protocol CryptoProtocol {
+    func encryptData() -> Result<Data, Error>?
+}
+
+// MOCK
+@main
+struct MockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MockView()
+        }
+    }
+}
+
+struct MockView: View {
+    @StateObject var mockCryptic = MockCryptic()
+    
+    var body: some View {
+        VStack {
+            Text("Hi")
+            
+            Text(mockCryptic.myData)
+                .background { Color.red }
+        }
+    }
+}
+
+final class MockCryptic: ObservableObject {
+//    let mySymmetricKey: SymmetricKey = .init(size: SymmetricKeySize(bitCount: 256))
+    @Published var myData: String = ""
+}
+
+extension MockCryptic: CryptoProtocol {
+    func encryptData() -> Result<Data, Error>? {
+        return nil
+    }
+    
+    
+}
+
+// MARK: Preview
+struct MockPreview: PreviewProvider {
+    static var previews: some View {
+        MockView()
+    }
+}
