b3cad13a42836996cf72f6aa2ad28af5e2f182d7
valselee <sollleky72@gmail.com>
2023-06-17 11:41:07 +0900

    [Update] Auth State 를 보관하는 Store 정의

--- Features/Auth/Sources/AppState/YTAuthStore.swift
+++ Features/Auth/Sources/AppState/YTAuthStore.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2023 Celan. All rights reserved.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+import Utilities
+
+public enum AuthState {
+    case signedIn
+    case beforeSignedin
+    case unsigned
+}
+
+public struct AuthStateStore: ReducerProtocol {
+    public struct State: Equatable {
+        
+    }
+    
+    public enum Action: Equatable {
+        case loginButtonTapped
+    }
+    
+    public init() { }
+    
+    public var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .loginButtonTapped:
+                return .none
+            }
+        }
+    }
+}
