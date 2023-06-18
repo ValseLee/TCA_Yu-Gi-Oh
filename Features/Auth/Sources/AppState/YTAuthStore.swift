6839519ee6bb6a418a88820b1187615476ab0460
valselee <sollleky72@gmail.com>
2023-06-18 22:15:22 +0900

    [Feat] 1. Auth 인증 상태와 로그인 상태를 구별하는 열거형 정의 2. 이벤트 스토밍 진행 필요

--- Features/Auth/Sources/AppState/YTAuthStore.swift
+++ Features/Auth/Sources/AppState/YTAuthStore.swift
@@ -10,18 +10,31 @@ import ComposableArchitecture
 import SwiftUI
 import Utilities
 
-public enum AuthState {
-    case signedIn
-    case beforeSignedin
-    case unsigned
+/**
+ User가 로그인 되어 있는지, 로그인되지 않았는지를 구별하는 열거형
+ */
+public enum AuthProcessState {
+    case unSigned, beforeSignedin, signedIn
+}
+
+/**
+ 로그인 인증 상태를 구별해주는 열거형
+ */
+public enum AuthenticateState {
+    case prepare, success, wrongPassword, wrongID
 }
 
 public struct AuthStateStore: ReducerProtocol {
     public struct State: Equatable {
+        public var authProcessState: AuthProcessState = .unSigned
+        public var authenticateState: AuthenticateState = .prepare
+        public var isLoginButtonTapped: Bool = false
         
     }
     
     public enum Action: Equatable {
+        case checkAuthenticate
+        case changeAuthProcessState
         case loginButtonTapped
     }
     
@@ -30,8 +43,16 @@ public struct AuthStateStore: ReducerProtocol {
     public var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
-            case .loginButtonTapped:
+            case .checkAuthenticate:
+                // 유저가 로그인 되어 있는지를 확인한다.
+                // 자동 로그인이 되어있다면 success로 state를 변경
                 return .none
+                
+            case .changeAuthProcessState:
+                return .none
+                
+            case .loginButtonTapped:
+                return .send(.changeAuthProcessState)
             }
         }
     }
