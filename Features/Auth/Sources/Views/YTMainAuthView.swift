539c55afea6f426bf28900ca68721186c6155ead
valselee <sollleky72@gmail.com>
2023-06-18 22:14:38 +0900

    [Chore] 파일 이름 수정

---
+++ Features/Auth/Sources/Views/YTMainAuthView.swift
@@ -0,0 +1,21 @@
+//
+//  YTMainAuthView.swift
+//  YugiTraderAuth
+//
+//  Created by Celan on 2023/06/18.
+//  Copyright © 2023 Celan. All rights reserved.
+//
+
+import SwiftUI
+
+struct YTMainAuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YTMainAuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        YTMainAuthView()
+    }
+}
