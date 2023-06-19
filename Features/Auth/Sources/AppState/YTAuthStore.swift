3e401fdcc2e4918c5a539f17c2f0c23ad4e3c6b4
valselee <sollleky72@gmail.com>
2023-06-19 23:11:26 +0900

    [Update] 의사 코드 일부 추가

--- Features/Auth/Sources/AppState/YTAuthStore.swift
+++ Features/Auth/Sources/AppState/YTAuthStore.swift
@@ -21,21 +21,34 @@ public enum AuthProcessState {
  로그인 인증 상태를 구별해주는 열거형
  */
 public enum AuthenticateState {
-    case prepare, success, wrongPassword, wrongID
+    case prepared, success, wrongID, wrongPassword
 }
 
 public struct AuthStateStore: ReducerProtocol {
     public struct State: Equatable {
+        public static func == (
+            lhs: AuthStateStore.State,
+            rhs: AuthStateStore.State
+        ) -> Bool {
+            lhs.userDeviceToken == rhs.userDeviceToken
+        }
+        
+        public var userDeviceToken: String = UIDevice.current.identifierForVendor!.uuidString
         public var authProcessState: AuthProcessState = .unSigned
-        public var authenticateState: AuthenticateState = .prepare
+        public var authenticateState: AuthenticateState = .prepared
+        
         public var isLoginButtonTapped: Bool = false
         
+        @AppStorage("HasUserLoggedIn")
+        var hasUserLoggedIn: Bool = UserDefaults.standard.bool(forKey: "")
     }
     
     public enum Action: Equatable {
         case checkAuthenticate
         case changeAuthProcessState
+        
         case loginButtonTapped
+        case loginScreenIsAppearing
     }
     
     public init() { }
@@ -43,17 +56,60 @@ public struct AuthStateStore: ReducerProtocol {
     public var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
+            case .loginScreenIsAppearing:
+                return .none
+                
             case .checkAuthenticate:
                 // 유저가 로그인 되어 있는지를 확인한다.
                 // 자동 로그인이 되어있다면 success로 state를 변경
+                Task {
+                    await checkAuthenticate()
+                }
                 return .none
                 
             case .changeAuthProcessState:
-                return .none
+                switch state.authenticateState {
+                case .prepared:
+                    return .none
+                    
+                case .success:
+                    if state.isLoginButtonTapped {
+                        state.isLoginButtonTapped = false
+                    }
+                    return .none
+                    
+                default:
+                    return .none
+//                    return .send(.wrongAuthenticationInformation)
+                    
+                }
                 
             case .loginButtonTapped:
-                return .send(.changeAuthProcessState)
+                state.isLoginButtonTapped = true
+                Task {
+                    await processAppleLogin()
+                }
+                
+                return .none
+                
+//            case .wrongAuthenticationInformation:
+//
+//                return .none
             }
         }
+    }
+    
+    private func processAppleLogin() async {
+        // Apple Login 실행
+        // Auth 내부에 Capability 추가 필요
+    }
+    
+    private func checkAuthenticate() async {
+        // 만약 서버에 user의 Token이 있다 -> success
+        // 없다 -> login 진행
+    }
+    
+    private func createUserOnServer() async {
+        // 서버에 유저 정보 생성
     }
 }
