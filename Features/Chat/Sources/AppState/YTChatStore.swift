e3c439ccb449e16d29e4e16d02e8da5d4f29986a
valselee <sollleky72@gmail.com>
2023-06-25 18:31:33 +0900

    [Feat] 1. Action 분리 -> Render, Background 2. Scroll, Alignment state 를 Store가 보관하도록 수정

--- Features/Chat/Sources/AppState/YTChatStore.swift
+++ Features/Chat/Sources/AppState/YTChatStore.swift
@@ -8,67 +8,137 @@
 
 import SwiftUI
 import ComposableArchitecture
-import YugiTraderAppInternal
-
-public enum StateFilter: String {
-    case all, proceeding, completed
-}
+import Utilities
+import AppCore
 
 public struct ChatStateStore: ReducerProtocol {
+    // MARK: STATE Filter
+    public enum ChatStateFilter: String {
+        case all, proceeding, completed
+    }
+    
+    // MARK: STATE
     public struct State: Equatable {
-        public var chatStateFilter: StateFilter = .all
-        public var name: String
-        public var scollDate: Date
-        public var scrollHighlight: String
+        public var chatStateFilter: ChatStateFilter = .all
+        public var currentUserName: String
+        public var scrollDate: Date
+        public var scrollHighlighted: String
         public var isCalendarShown: Bool
         
+        // Message Bubble UI State
+        public var messageBubbleAlignment: Alignment
+        public var messageBubbleHorizontalAlignment: HorizontalAlignment
+        public var messageBubbleBackgroundColor: Color
+        
+        // MARK: - State Lifecycle
         public init(
-            chatStateFilter: StateFilter,
-            name: String,
-            scollDate: Date,
+            chatStateFilter: ChatStateFilter,
+            currentUserName: String,
+            scrollDate: Date,
             scrollHighlight: String,
-            isCalendarShown: Bool
+            isCalendarShown: Bool,
+            messageBubbleAlignment: Alignment,
+            messageBubbleHorizontalAlignment: HorizontalAlignment,
+            messageBubbleBackgroundColor: Color
         ) {
             self.chatStateFilter = chatStateFilter
-            self.name = name
-            self.scollDate = scollDate
-            self.scrollHighlight = scrollHighlight
+            self.currentUserName = currentUserName
+            self.scrollDate = scrollDate
+            self.scrollHighlighted = scrollHighlight
             self.isCalendarShown = isCalendarShown
+            self.messageBubbleAlignment = messageBubbleAlignment
+            self.messageBubbleHorizontalAlignment = messageBubbleHorizontalAlignment
+            self.messageBubbleBackgroundColor = messageBubbleBackgroundColor
+        }
+    }
+    // MARK: Action
+    public enum Action: Equatable {
+        case renderAction(ViewRenderAction)
+        case backgroundLogics(BackgroundAction)
+    }
+    
+    public enum ViewRenderAction: Equatable {
+        case renderUserMessageBubbleWithAlignment(message: Message)
+        case renderUserMessageBubbleWithBackgroundColor(message: Message)
+        case scrollTo(proxy: ScrollViewProxy, position: String)
+        case highLightMessageBubble
+        
+        private var id: UUID { UUID() }
+        public static func == (
+            lhs: ChatStateStore.ViewRenderAction,
+            rhs: ChatStateStore.ViewRenderAction
+        ) -> Bool {
+            lhs.id == rhs.id
         }
     }
     
-    public enum Action: Equatable {
-        case sendMessage
-        case afterSendMessage
-        case selectDateToScroll(message: Message)
-        case scrollToSpecificDate(message: Message)
-        case highlightMessageBubble(message: Message)
+    public enum BackgroundAction: Equatable {
+        case sendMessage(Message)
     }
     
+    // MARK: STORE LIFECYCLE
     public init() { }
     
+    // MARK: Reducer
     public var body: some ReducerProtocol<State, Action> {
         Reduce { state, action in
             switch action {
-            case .sendMessage:
-                state.chatStateFilter = .proceeding
-                print("Message IS NOW SENDING")
-                return .send(.afterSendMessage)
+            // MARK: VIEW RENDER ACTION
+            case let .renderAction(renderAction):
+                switch renderAction {
+                case let .renderUserMessageBubbleWithAlignment(message):
+                    if state.currentUserName == message.messageSenderID {
+                        state.messageBubbleAlignment = .trailing
+                        state.messageBubbleHorizontalAlignment = .trailing
+                    } else {
+                        state.messageBubbleAlignment = .leading
+                        state.messageBubbleHorizontalAlignment = .leading
+                    }
+                    
+                    return .none
                 
-            case .afterSendMessage:
-                state.chatStateFilter = .completed
-                return .none
+                case let .renderUserMessageBubbleWithBackgroundColor(message):
+                    let dateString = Date.getMessageDateString(with: message.date)
+                    
+                    if state.scrollHighlighted == dateString {
+                        state.messageBubbleBackgroundColor = Color.yellow
+                    } else if state.currentUserName == message.messageSenderID {
+                        state.messageBubbleBackgroundColor = .blue
+                    } else {
+                        state.messageBubbleBackgroundColor = .yellow
+                    }
+                    
+                    return .none
+                    
+                case let .scrollTo(reader, position):
+                    state.scrollHighlighted = position
+                    
+                    withAnimation {
+                        reader.scrollTo(
+                            state.scrollHighlighted,
+                            anchor: .bottom
+                        )
+                    }
+                    
+                    return .send(.renderAction(.highLightMessageBubble))
+                    
+                case .highLightMessageBubble:
+                    withAnimation(Animation.easeInOut(duration: 1.25)) {
+                        state.scrollHighlighted = ""
+                    }
+                    
+                    return .none
+                }
             
-            case let .selectDateToScroll(message):
-                return .send(.scrollToSpecificDate(message: message))
-                
-            case let .scrollToSpecificDate(message):
-                state.scollDate = message.date
-                return .send(.highlightMessageBubble(message: message))
-                
-            case let .highlightMessageBubble(message):
-                state.scrollHighlight = message.getSpecificDateStringToScroll()
-                return .none
+            // MARK: BACKGROUND LOGICS
+            case let .backgroundLogics(background):
+                switch background {
+                case let .sendMessage(message):
+                    state.chatStateFilter = .proceeding
+                    return .run { send in
+                        print("CREATE A \(message.messageSenderID) ON FIRESTORE")
+                    }
+                }
             }
         }
     }
