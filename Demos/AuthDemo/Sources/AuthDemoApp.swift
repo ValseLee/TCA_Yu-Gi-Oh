11ea5ad445a56fa3caf700e9a0a3a84a31637571
valselee <sollleky72@gmail.com>
2023-06-27 00:27:34 +0900

    [Feat] Demo Projects 1. Chat, Auth Demo Mock File Added

---
+++ Demos/AuthDemo/Sources/AuthDemoApp.swift
@@ -0,0 +1,21 @@
+//
+//  AuthDemoApp.swift
+//  AuthDemo
+//
+//  Created by Celan on 2023/06/27.
+//  Copyright © 2023 Celan. All rights reserved.
+//
+
+import SwiftUI
+
+struct AuthDemoApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthDemoApp_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthDemoApp()
+    }
+}
